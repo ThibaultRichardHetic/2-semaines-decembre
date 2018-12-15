56016d1cc713f99135cfa7715c05bd19602081bc
PaulHaddou <paul2405.63@hotmail.fr>
2018-12-15 22:01:50 +0100

    :construction: Added sidebar icons. Added the user icon in the navbar. Added post blocks. Added footer. Page ~90% finished

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       </form>
       <div class="navbar__loupe"></div>
       <div class="navbar__logo"></div>
-      <div class="navbar__profil"></div>
+      <div class="navbar__profil"><img src="src/Navbar/User.png"></div>
     </div>
     <div class="container__sidebar">
       <div class="sidebar__avatarpicture">
@@ -26,23 +26,62 @@
       <div class="sidebar__topics">
           <ul>
             <li class="topics__mytopics"><a href="#">Mes Thèmes</a></li>
-            <li class="topics__videos"><a href="#">Vidéos</a></li>
-            <li class="topics__sport"><a href="#">Sport</a></li>
-            <li class="topics__videosgames"><a href="#">Jeux Vidéos</a></li>
-            <li class="topics__art"><a href="#">Art</a></li>
-            <li class="topics__music"><a href="#">Musique</a></li>
-            <li class="topics__myclubs"><a href="#">Mes Clubs</a></li>
+            <li class="topics__videos"><a href="#">Musique</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Music.png" class="topics__iconmusic"></li>
+            <li class="topics__sport"><a href="#">Sport</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Sport.png" class="topics__iconsport"></li>
+            <li class="topics__videosgames"><a href="#">Jeux</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Controller.png" class="topics__iconcontroller"></li>
+            <li class="topics__art"><a href="#">Art</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Pencil.png" class="topics__iconpencil"></li>
+            <li class="topics__music"><a href="#">Cuisine</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Cook.png" class="topics__iconcook"></li>
+            <li class="topics__tutoriels"><a href="#">Tutoriels</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Scissors.png" class="topics__iconscissors"></li>
+            <li class="topics__humor"><a href="#">Humour</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Tongue.png" class="topics__icontongue"></li>
+            <li class="topics__myclubs"><a href="#">Mes Clubs</a><img src="src/Sidebar/Cross.png"></li>
           </ul>
       </div>
     </div>
     <div class="main">
-      <img class="main__backgroundimg" src="src/background_space.png" alt="image représentant l'espace avec des planètes. Servant de d'image de fond au fil découverte">
+      <div class="main__backgroundimg"></div>
       <img class="main__spacemanimg" src="src/illu_astro-feed.svg" alt="image représentant l'espace avec des planètes. Servant de d'image de fond au fil découverte">
       <div class="main__title">
         <h2>Découverte</h2>
       </div>
+      <div class="main__posts">
+        <div class="posts__post1"></div>
+        <div class="posts__post2"></div>
+        <div class="posts__post3"></div>
+        <div class="posts__post4"></div>
+        <div class="posts__post5"></div>
+        <div class="posts__post6"></div>
+        <div class="posts__post7"></div>
+        <div class="posts__post8"></div>
+        <div class="posts__post9"></div>
+        <div class="posts__post10"></div>
+        <div class="posts__post11"></div>
+        <div class="posts__post12"></div>
+        <div class="posts__post13"></div>
+        <div class="posts__post14"></div>
+        <div class="posts__post15"></div>
+        <div class="posts__post16"></div>
+        <div class="posts__post17"></div>
+        <div class="posts__post18"></div>
+        <div class="posts__post19"></div>
+        <div class="posts__post20"></div>
+        <div class="posts__post21"></div>
+        <div class="posts__post22"></div>
+        <div class="posts__post23"></div>
+        <div class="posts__post24"></div>
+      </div>
     </div>
   </div>
-  <script src="scripts/app.js" > </script>
+  <footer>
+    <ul>
+      <li class="footer__aboutus"><a href="#">à propos de nous</a></li>
+      <li class="footer__support"><a href="#">Support</a></li>
+      <li class="footer__api"><a href="#">API</a></li>
+      <li class="footer__confidentiality "><a href="#">Confidentialité</a></li>
+      <li class="footer__conditions"><a href="#">Conditions</a></li>
+      <li class="footer__profil"><a href="#">Profil</a></li>
+      <li class="footer__language"><a href="#">Langue</a></li>
+    </ul>
+  </footer>
+  <script src="scripts/app.js"></script>
 </body>
 </html>
