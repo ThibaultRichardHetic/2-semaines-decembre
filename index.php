39e23b45f1baf3f5523ea3edd3e56609cf938233
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-12-17 01:07:11 +0100

    :rocket: creation of landing page with the message for users

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>2 semaines decembre</title>
+    <title>CreHappy</title>
     <link href="https://fonts.googleapis.com/css?family=Bungee|Josefin+Sans" rel="stylesheet">
     <link rel="stylesheet" href="styles/reset.min.css">
     <link rel="stylesheet" href="styles/style.min.css">
 </head>
-<body>
+<body class="body--index">
   <div class="container">
     <div class="container__navbar">
       <form class="navbar__research" method="post">
