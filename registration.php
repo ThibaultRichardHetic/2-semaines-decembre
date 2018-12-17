ddfe5efe1a7c6671a666a41dc78bf759bf0c411d
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-12-17 10:31:41 +0100

    :bug: overflow x fixed for home page

---
+++ registration.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CreHappy</title>
+    <link href="https://fonts.googleapis.com/css?family=Bungee|Josefin+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="styles/reset.min.css">
+    <link rel="stylesheet" href="styles/style.min.css">
+</head>
+<body class="body--registration">
+
+  <main>
+    <section>
+      <div>
+        <img src="" alt="">
+        <div>
+          <a href=""></a>
+        </div>
+      </div>
+      <div>
+        <h1>Je crée mon compte</h1>
+      </div>
+    </section>
+
+  </main>
+  
+  <footer>
+
+  </footer>
+
+  <script src="scripts/app.js"></script>
+</body>
+</html>
