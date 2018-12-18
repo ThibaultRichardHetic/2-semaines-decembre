958779550e3375c28efc8a49ab585eebf144a966
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-12-18 13:51:08 +0100

    :construction: landing page button hover and paralax

--- landing.php
+++ landing.php
@@ -16,7 +16,7 @@
     <div class="button--fixed">
       <div class="button__register">
         <p>
-          je m’inscris
+          <a href="register.php">je m’inscris</a>
         </p>
       </div>
       <p class="register__interrogation">
@@ -24,68 +24,68 @@
       </p>
       <div class="button__connexion">
         <p>
-          Se connecter
+          <a href="#">Se connecter</a>
         </p>
       </div>
     </div>
     <div class="img--fixed">
-      <img src="src/landing/planet.png" alt="">
-
+      <img src="src/landing/planet.png" data-cursor-depth="0.005" class="cursor-parallax lazy" alt="">
     </div>
   </header>
   <main id="fullpage">
     
     <section class="section--step1 section--default section">
-      <div class="step1__text text-container--default">
+      <div class="step1__text text-container--default cursor-parallax lazy" data-cursor-depth="0.020">
         <img src="src/landing/bulle03.svg" alt="">
         <h3>Bienvenue à toi !</h3>
       </div>
-      <img class="men--default men--right" src="src/landing/illu_astro-accueil.png" alt="">
+      <img class="men--default men--right cursor-parallax lazy" data-cursor-depth="0.040" data-cursor-depth="0.040" src="src/landing/illu_astro-accueil.png" alt="">
     </section>
     <section class="section--step2 section--default section">
-      <div class="step2__text text-container--default">
+      <div class="step2__text text-container--default cursor-parallax lazy" data-cursor-depth="0.020">
         <img src="src/landing/bulle02.svg" alt="">
         <p>
            Ici tu t'inscris sur un espace de <span class="important--red">partage</span>, <span class="important--red">liberté</span>, <span class="important--red">positivité</span>, <span class="important--red">sécurité</span> et surtout : <span class="important--red important--bold">créativité</span>. C'est aussi ici que tu pourra retrouver tes amis et jeunes de ton âge.
         </p>
       </div>
-      <img class="men--default" src="src/landing/illu_astro-accueil2.svg" alt="">
+      <img class="men--default cursor-parallax lazy" src="src/landing/illu_astro-accueil2.svg" data-cursor-depth="0.040" alt="">
     </section>
     <section class="section--step3 section--default section">
-      <div class="step3__text text-container--default">
-        <img src="src/landing/bulle03.svg" alt="">
+      <div class="step3__text text-container--default cursor-parallax lazy" data-cursor-depth="0.020">
+        <img src="src/landing/bulle03.svg" 
+          alt="">
         <p>
           Bientôt tu vas avoir <span class="important">ton journal</span> et ton fil de <span class="important">découverte</span>. Un journal <span class="important">public</span> où ce que tu met sera visible par tous, un journal <span class="important">privé</span> que tu seras seul à voir, et un fil de découverte qui te proposera des <span class="important important--bold">créas</span> qui te correspondent.
         </p>
       </div>
-      <img class="men--default men--right" src="src/landing/illu_astro-accueil3.svg" alt="">
+      <img class="men--default men--right cursor-parallax lazy" data-cursor-depth="0.040" src="src/landing/illu_astro-accueil3.svg" alt="">
     </section>
     <section class="section--step4 section--default section">
-      <div class="step4__text text-container--default">
+      <div class="step4__text text-container--default cursor-parallax lazy" data-cursor-depth="0.020">
         <img src="src/landing/bulle02.svg" alt="">
         <p>
           Tu peux aussi jouer à des <span class="important--red">jeux</span> et même consulter les <span class="important--red important--bold">créas</span> des autres sur des sujets trop cools !        
         </p>
       </div>
-      <img class="men--default" src="src/landing/illu_astro-accueil2.svg" alt="">
+      <img class="men--default cursor-parallax lazy" src=" src/landing/illu_astro-accueil2.svg"  data-cursor-depth="0.040" alt="">
     </section>
     <section class="section--step5 section--default section">
-      <div class="step5__text text-container--default">
+      <div class="step5__text text-container--default cursor-parallax lazy" data-cursor-depth="0.020">
         <img src="src/landing/bulle03.svg" alt="">
         <p>
           Fais quand même attention! Il y a peu de règles mais une super importante : le <span class="important important--bold">respect</span> des autres. Donc pas de commentaire méchant, pas de gros mots ou d’insultes !        
         </p>
       </div>
-      <img class="men--default  men--right" src="src/landing/illu_astro-accueil4.svg" alt="">
+      <img class="men--default men--right cursor-parallax lazy" data-cursor-depth="0.040" src="src/landing/illu_astro-accueil4.svg" alt="">
     </section>
     <section class="section--step6 section--default section">
-      <div class="step6__text text-container--default">
+      <div class="step6__text text-container--default cursor-parallax lazy" data-cursor-depth="0.020">
         <img src="src/landing/bulle02.svg" alt="">
         <p>
           Amuse toi, ta seule limite est ton imagination !        
         </p>
       </div>
-      <img class="men--default" src="src/landing/illu_astro-accueil5.svg" alt="">
+      <img class="men--default cursor-parallax lazy" src="src/landing/illu_astro-accueil5.svg" data-cursor-depth="0.040" alt="">
     </section>
    
   </main>
