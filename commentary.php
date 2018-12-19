f5732e87b0ccb9c6927eebfc4c5f26bb1ea87a93
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-12-19 23:35:27 +0100

    :sparkles: add favicon

--- commentary.php
+++ commentary.php
@@ -5,88 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CreHappy</title>
+    <link rel="icon" type="image/png" href="src/all/logo.png"/>
     <link href="https://fonts.googleapis.com/css?family=Bungee|Josefin+Sans" rel="stylesheet">
     <link rel="stylesheet" href="styles/reset.min.css">
     <link rel="stylesheet" href="styles/style.min.css">
+    favic
 </head>
 <body class="body--commentary">
-  <div class="container">
-    <div class="container__navbar">
-      <div class="navbar__reasearch">
-        <form class="reasearch__form" method="post">
-          <input name="saisie" type="text" placeholder="Recherche" required />
-        </form>
-        <div class="navbar__magnifyingglass">
-          <a href="#">
-            <img src="src/Navbar/Glass.png">
-          </a>
-        </div>
-      </div>
-      <div class="navbar__logo">
-        <a href="index.php">
-          <img src="src/all/logo.png" alt="">
-        </a>  
-      </div>
-      <div class="navbar__profil">
-        <a href="myjournal.php">
-          <img src="src/Navbar/User.png">
-        </a>
-      </div>
-    </div>
-    <div class="container__sidebar">
-      <div class="sidebar__avatarpicture">
-        <a href="myjournal.php">
-          <img class="js-avatarpicture" src="src/Discovery/Main/Posts/PP/images.png" alt="avatar représentant un utilisateur">
-        </a>     
-        <div class="avatarpicture__pseudo"><p>Pseudo</p></div>
-      </div>
-      <div class="sidebar__topics">
-          <ul>
-            <li class="topics__mytopics">
-              <a href="#">Mes Thèmes</a>
-            </li>
-            <li class="topics__videos">
-              <a href="#">Musique</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Music.png" class="topics__iconmusic">
-            </li>
-            <li class="topics__sport">
-              <a href="#">Sport</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Sport.png" class="topics__iconsport">
-            </li>
-            <li class="topics__videosgames">
-              <a href="#">Jeux</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Controller.png" class="topics__iconcontroller">
-            </li>
-            <li class="topics__art">
-              <a href="#">Art</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Pencil.png" class="topics__iconpencil">
-            </li>
-            <li class="topics__music">
-              <a href="#">Cuisine</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Cook.png" class="topics__iconcook">
-            </li>
-            <li class="topics__tutoriels">
-              <a href="#">Tutoriels</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Scissors.png" class="topics__iconscissors">
-            </li>
-            <li class="topics__humor">
-              <a href="#">Humour</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Tongue.png" class="topics__icontongue">
-            </li>
-            <li class="topics__myclubs">
-              <a href="#">Mes Clubs</a>
-              <img class="js-add-club" src="src/Sidebar/Cross.png">
-            </li>
-          </ul>
-      </div>
-    </div>
+
+    <?php 
+    include 'header-side.php';
+    ?>
+
     <div class="main">
       <div class="main__backgroundimg"></div>
       <img class="main__spacemanimg" src="src/illu_astro-feed.svg" alt="image représentant l'espace avec des planètes. Servant de d'image de fond au fil découverte">
@@ -104,7 +34,7 @@
           </div>
           <div class="post1__form">
             <FORM>
-              <input wrap="virtual" name="nom" rows=4 cols=28% placeholder="Ton commentaire"/>
+              <input wrap="virtual" name="nom" rows=4 cols=28% placeholder="Ton commentaire..."/>
             </FORM>
           </div>
         </div>
--- register.php
+++ register.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CreHappy</title>
+    <link rel="icon" type="image/png" href="src/all/logo.png"/>
     <link href="https://fonts.googleapis.com/css?family=Bungee|Josefin+Sans" rel="stylesheet">
     <link rel="stylesheet" href="styles/reset.min.css">
     <link rel="stylesheet" href="styles/style.min.css">
