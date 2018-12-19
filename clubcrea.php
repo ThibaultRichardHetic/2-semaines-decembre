11bb1e5f316db091fd23d6b81aa672ba535776c6
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-12-19 12:55:57 +0100

    :art: fixe road to image and indentattion

--- clubcrea.php
+++ clubcrea.php
@@ -15,32 +15,41 @@
             <form class="navbar__research" method="post">
                 <input name="saisie" type="text" placeholder="Recherche" required />
             </form>
-        <div class="navbar__magnifyingglass"><a href="#"><img src="src/Navbar/Glass.png"></a></div>
-        <div class="navbar__logo"></div>
-        <div class="navbar__profil"><a href="myjournal.php"><img src="src/Navbar/User.png"></a></div>
-    </div>
-    <div class="container__sidebar">
-        <div class="sidebar__avatarpicture">
-            <a href="myjournal.php">
-                <img class="js-avatarpicture" src="src/Discovery/Main/Posts/PP/images.png" alt="">
-            </a>
-            <div class="avatarpicture__pseudo">
-                <p>Pseudo</p>
+            <div class="navbar__magnifyingglass">
+                <a href="#">
+                    <img src="src/Navbar/Glass.png">
+                </a>
             </div>
-      </div>
-      <div class="sidebar__topics">
-          <ul class="js-ul-side">
-            <li class="topics__mytopics"><a href="#">Mes Thèmes</a></li>
-            <li class="topics__videos"><a href="#">Musique</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Music.png" class="topics__iconmusic"></li>
-            <li class="topics__sport"><a href="#">Sport</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Sport.png" class="topics__iconsport"></li>
-            <li class="topics__videosgames"><a href="#">Jeux</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Controller.png" class="topics__iconcontroller"></li>
-            <li class="topics__art"><a href="#">Art</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Pencil.png" class="topics__iconpencil"></li>
-            <li class="topics__music"><a href="#">Cuisine</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Cook.png" class="topics__iconcook"></li>
-            <li class="topics__tutoriels"><a href="#">Tutoriels</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Scissors.png" class="topics__iconscissors"></li>
-            <li class="topics__humor"><a href="#">Humour</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Tongue.png" class="topics__icontongue"></li>
-            <li class="topics__myclubs"><a href="#">Mes Clubs</a><img src="src/Sidebar/Cross.png"></li>
-          </ul>
-      </div>
+            <img src="src/all/logo.png" class="nav__logo" alt="">
+            <div class="navbar__profil">
+                <a href="myjournal.php">
+                    <img src="src/Navbar/User.png">
+                </a>
+            </div>
+        </div>
+        <div class="container__sidebar">
+            <div class="sidebar__avatarpicture">
+                <a href="myjournal.php">
+                    <img class="js-avatarpicture" src="src/Discovery/Main/Posts/PP/images.png" alt="">
+                </a>
+                <div class="avatarpicture__pseudo">
+                    <p>Pseudo</p>
+                </div>
+            </div>
+            <div class="sidebar__topics">
+                <ul class="js-ul-side">
+                    <li class="topics__mytopics"><a href="#">Mes Thèmes</a></li>
+                    <li class="topics__videos"><a href="#">Musique</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Music.png" class="topics__iconmusic"></li>
+                    <li class="topics__sport"><a href="#">Sport</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Sport.png" class="topics__iconsport"></li>
+                    <li class="topics__videosgames"><a href="#">Jeux</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Controller.png" class="topics__iconcontroller"></li>
+                    <li class="topics__art"><a href="#">Art</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Pencil.png" class="topics__iconpencil"></li>
+                    <li class="topics__music"><a href="#">Cuisine</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Cook.png" class="topics__iconcook"></li>
+                    <li class="topics__tutoriels"><a href="#">Tutoriels</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Scissors.png" class="topics__iconscissors"></li>
+                    <li class="topics__humor"><a href="#">Humour</a><img src="src/Sidebar/Hexagon_White.png"><img src="src/Sidebar/Tongue.png" class="topics__icontongue"></li>
+                    <li class="topics__myclubs"><a href="#">Mes Clubs</a><img src="src/Sidebar/Cross.png"></li>
+                </ul>
+            </div>
+        </div>
     </div>
     <main>
         <section class="section--step1">
