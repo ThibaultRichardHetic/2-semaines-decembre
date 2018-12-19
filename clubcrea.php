4753b40df7c37e71ff07c739a9202ba78c7f93b9
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-12-19 16:02:33 +0100

    :sparkles: segmentation files creation header.php

--- clubcrea.php
+++ clubcrea.php
@@ -10,86 +10,11 @@
     <link rel="stylesheet" href="styles/style.min.css">
 </head>
 <body class="body--clubcrea">
-    <div class="container">
-        <div class="container__navbar">
-            <div class="navbar__reasearch">
-                <form class="reasearch__form" method="post">
-                <input name="saisie" type="text" placeholder="Recherche" required />
-                </form>
-                <div class="navbar__magnifyingglass">
-                <a href="#">
-                    <img src="src/Navbar/Glass.png">
-                </a>
-                </div>
-            </div>
-            <div class="navbar__logo">
-                <a href="index.php">
-                <img src="src/all/logo.png" alt="">
-                </a>  
-            </div>
-            <div class="navbar__profil">
-                <a href="myjournal.php">
-                <img src="src/Navbar/User.png">
-                </a>
-            </div>
-        </div>
-        <div class="container__sidebar">
-            <div class="sidebar__avatarpicture">
-                <a href="myjournal.php">
-                    <img class="js-avatarpicture" src="src/Discovery/Main/Posts/PP/images.png" alt="">
-                </a>
-                <div class="avatarpicture__pseudo">
-                    <p>Pseudo</p>
-                </div>
-            </div>
-            <div class="sidebar__topics">
-                <ul class="js-ul-side">
-                    <li class="topics__mytopics">
-                        <a href="#">Mes Thèmes</a>
-                    </li>
-                    <li class="topics__videos">
-                        <a href="#">Musique</a>
-                        <img src="src/Sidebar/Hexagon_White.png">
-                        <img src="src/Sidebar/Music.png" class="topics__iconmusic">
-                    </li>
-                    <li class="topics__sport">
-                        <a href="#">Sport</a>
-                        <img src="src/Sidebar/Hexagon_White.png">
-                        <img src="src/Sidebar/Sport.png" class="topics__iconsport">
-                    </li>
-                    <li class="topics__videosgames">
-                        <a href="#">Jeux</a>
-                        <img src="src/Sidebar/Hexagon_White.png">
-                        <img src="src/Sidebar/Controller.png" class="topics__iconcontroller">
-                    </li>
-                    <li class="topics__art">
-                        <a href="#">Art</a>
-                        <img src="src/Sidebar/Hexagon_White.png">
-                        <img src="src/Sidebar/Pencil.png" class="topics__iconpencil">
-                    </li>
-                    <li class="topics__music">
-                        <a href="#">Cuisine</a>
-                        <img src="src/Sidebar/Hexagon_White.png">
-                        <img src="src/Sidebar/Cook.png" class="topics__iconcook">
-                    </li>
-                    <li class="topics__tutoriels">
-                        <a href="#">Tutoriels</a>
-                        <img src="src/Sidebar/Hexagon_White.png">
-                        <img src="src/Sidebar/Scissors.png" class="topics__iconscissors">
-                    </li>
-                    <li class="topics__humor">
-                        <a href="#">Humour</a>
-                        <img src="src/Sidebar/Hexagon_White.png">
-                        <img src="src/Sidebar/Tongue.png" class="topics__icontongue">
-                    </li>
-                    <li class="topics__myclubs">
-                        <a href="#">Mes Clubs</a>
-                        <img src="src/Sidebar/Cross.png">
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+
+    <?php 
+    include 'header-side.php';
+    ?>
+
     <main>
         <section class="section--step1">
             <h3>Nouveau club</h3>
--- index.php
+++ index.php
@@ -10,83 +10,10 @@
     <link rel="stylesheet" href="styles/style.min.css">
 </head>
 <body class="body--index">
-  <div class="container">
-    <div class="container__navbar">
-      <div class="navbar__reasearch">
-        <form class="reasearch__form" method="post">
-          <input name="saisie" type="text" placeholder="Recherche" required />
-        </form>
-        <div class="navbar__magnifyingglass">
-          <a href="#">
-            <img src="src/Navbar/Glass.png">
-          </a>
-        </div>
-      </div>
-      <div class="navbar__logo">
-        <a href="index.php">
-          <img src="src/all/logo.png" alt="">
-        </a>  
-      </div>
-      <div class="navbar__profil">
-        <a href="myjournal.php">
-          <img src="src/Navbar/User.png">
-        </a>
-      </div>
-    </div>
-    <div class="container__sidebar">
-      <div class="sidebar__avatarpicture">
-        <a href="myjournal.php">
-          <img class="js-avatarpicture" src="src/Discovery/Main/Posts/PP/images.png" alt="avatar représentant un utilisateur">
-        </a>     
-        <div class="avatarpicture__pseudo"><p>Pseudo</p></div>
-      </div>
-      <div class="sidebar__topics">
-          <ul>
-            <li class="topics__mytopics">
-              <a href="#">Mes Thèmes</a>
-            </li>
-            <li class="topics__videos">
-              <a href="#">Musique</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Music.png" class="topics__iconmusic">
-            </li>
-            <li class="topics__sport">
-              <a href="#">Sport</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Sport.png" class="topics__iconsport">
-            </li>
-            <li class="topics__videosgames">
-              <a href="#">Jeux</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Controller.png" class="topics__iconcontroller">
-            </li>
-            <li class="topics__art">
-              <a href="#">Art</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Pencil.png" class="topics__iconpencil">
-            </li>
-            <li class="topics__music">
-              <a href="#">Cuisine</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Cook.png" class="topics__iconcook">
-            </li>
-            <li class="topics__tutoriels">
-              <a href="#">Tutoriels</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Scissors.png" class="topics__iconscissors">
-            </li>
-            <li class="topics__humor">
-              <a href="#">Humour</a>
-              <img src="src/Sidebar/Hexagon_White.png">
-              <img src="src/Sidebar/Tongue.png" class="topics__icontongue">
-            </li>
-            <li class="topics__myclubs">
-              <a href="#">Mes Clubs</a>
-              <img class="js-add-club" src="src/Sidebar/Cross.png">
-            </li>
-          </ul>
-      </div>
-    </div>
+    <?php 
+    include 'header-side.php';
+    ?>
+
     <div class="main">
       <div class="main__backgroundimg"></div>
       <img class="main__spacemanimg" src="src/illu_astro-feed.svg" alt="image représentant l'espace avec des planètes. Servant de d'image de fond au fil découverte">
